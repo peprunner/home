9c151bc0d1d74b6a02806f462c025543e406792c
peprunner <39007193+peprunner@users.noreply.github.com>
2022-01-16 00:59:01 +0100

    Remove typos

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -42,7 +42,7 @@ const about = {
   imageLink: require("../editable-stuff/photo.jpg"),
   imageSize: 375,
   message:
-    "My name is Giuseppe Andreetti. I’m a graduate of '17-'18 A.Y. from La Sapienza University in Rome with a degree in Enginnering in Computer Sciene. Java and C++ Developer. I am specialized in Software Engineering and Data Science. Well-versed in numerous programming languages including Java, Python, JavaScript. During my free time I am interested in crypto and DeFi",
+    "My name is Giuseppe Andreetti. I’m a graduate of 17-18 A.Y. from La Sapienza University in Rome with a degree in Enginnering in Computer Science. Java and C++ Developer. I am specialized in Software Engineering and Data Science. Well-versed in numerous programming languages including Java, Python, JavaScript. During my free time I am interested in crypto and DeFi",
   resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 };
 
